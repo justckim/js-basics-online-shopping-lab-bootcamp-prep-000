a09e08156ec30cb5030ec5f92afbe50590787023
Justin <jkimm516@gmail.com>
2018-04-24 05:04:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function removeFromCart(item) {
   for (var i = 0; i < myCart.length; i++) {
     
     if (myCart[i].itemName === item) {
-      myCart = [...myCart.slice(0, i)...myCart.slice(i+1, myCart.length)]
+      myCart = [...myCart.slice(0, i), ...myCart.slice(i+1, myCart.length)]
       itemIsInCart = true 
     }
   }
